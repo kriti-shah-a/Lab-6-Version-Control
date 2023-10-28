149efb6c5ac3b3327c22a2430a8aac46fef72771
Kriti <kritishah989@gmail.com>
2023-10-28 14:31:25 -0400

    formating

--- lab6.py
+++ lab6.py
@@ -1,32 +1,13 @@
 # Kriti Shah and Matthew Toro
 
-def main():
-    condition = True
-
-    while condition:
-        print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit')
-        user_input = int(input('Please enter an option: '))
-
-        if user_input == 1:
-            password = input('Please enter your password to encode: ')
-            encoded = encoder(password)
-            print('Your password has been encoded and stored!')
-            print()
-
-        if user_input == 2:
-            print('The encoded password is ' + str(encoded) + ', and the original password is ' + str(decoded(encoded)) + '.')
-            print()
-
-        if user_input == 3:
-            exit()
-
-
 def encoder(password):
     x = []
     y = ''
 
-    for i in range(0, len(password)):
-        x.append(int(password[i])+3)
+    for i in range(0, int(len(password))):
+        number = password[i]
+        number += 3
+        x.append(int(number))
 
     for i in x:
         y += str(i)
@@ -46,6 +27,25 @@ def decoded(password):
     return y
 
 
+def main():
+    condition = True
+
+    while condition:
+        print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit')
+        user_input = int(input('Please enter an option: '))
+
+        if user_input == 1:
+            password = input('Please enter your password to encode: ')
+            encoded = encoder(password)
+            print('Your password has been encoded and stored!')
+            print()
+
+        if user_input == 2:
+            print('The encoded password is ' + str(encoded) + ', and the original password is ' + str(decoded(encoded)) + '.')
+            print()
+
+        if user_input == 3:
+            exit()
 
 
 if __name__ == '__main__':
