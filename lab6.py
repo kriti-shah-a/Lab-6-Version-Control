a710fce94b26f9eedc9f6ae564540aa6deef6ed4
Matthew <matalext@gmail.com>
2023-10-28 14:32:41 -0400

    the code FINAL

--- lab6.py
+++ lab6.py
@@ -1,27 +1,27 @@
 # Kriti Shah and Matthew Toro
 
-def main():
+def main(): #Main
     condition = True
 
     while condition:
         print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit')
         user_input = int(input('Please enter an option: '))
 
-        if user_input == 1:
+        if user_input == 1: #User input 1
             password = input('Please enter your password to encode: ')
             encoded = encoder(password)
             print('Your password has been encoded and stored!')
             print()
 
-        if user_input == 2:
+        if user_input == 2: #User input 2
             print('The encoded password is ' + str(encoded) + ', and the original password is ' + str(decoded(encoded)) + '.')
             print()
 
-        if user_input == 3:
+        if user_input == 3: #Exit
             exit()
 
 
-def encoder(password):
+def encoder(password): #Encoder function, +3 to every digit
     x = []
     y = ''
 
@@ -33,7 +33,7 @@ def encoder(password):
 
     return y
 
-def decoded(password):
+def decoded(password): #Decoder function, -3 to every digit (inverse of encoder)
     x = []
     y = ''
 
