a81e6c6b3a139e7c84ae5b8269dbb0cd3c5614b1
Matthew <matalext@gmail.com>
2023-10-28 14:16:25 -0400

    the code 2

--- lab6.py
+++ lab6.py
@@ -8,14 +8,22 @@ def main():
         user_input = int(input('Please enter an option: '))
 
         if user_input == 1:
-            password = int(user_input('Please enter your password to encode: '))
-            password = encoder(passworod)
+            password = int(input('Please enter your password to encode: '))
+            password = encoder(password)
             print('Your password has been encoded and stored!')
             print()
 
+        if user_input == 2:
+            password = int(input('Please enter your password to decoded: '))
+            print('The encoded password is' + str(password) + ', and the original password is ' + str(decoder(password)) + '.')
+            print()
+
+        if user_input == 3:
+            exit()
+
 
 def encoder(password):
-    for i in range(0, len(password)):
+    for i in range(0, len(str(password))):
         if password[i].isdigit():
             password[i] += 3
 
