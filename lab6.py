1dcf8a2482bd8f8bf0f585d5918259d5a8c503fe
Kriti <kritishah989@gmail.com>
2023-10-30 11:51:39 -0400

    update

--- lab6.py
+++ lab6.py
@@ -25,29 +25,4 @@ def decoded(password):  # Decoder function, inverse of encoder
 
     return y  # return the string that has subtracted 3 for each number
 
-def main():
-    while True:  # Loop function
-        print('Menu')
-        print('-------------')
-        print('1. Encode')
-        print('2. Decode')
-        print('3. Quit')  # Print the Menu
-        user_input = int(input('Please enter an option: '))  # asking for user input
-
-        if user_input == 1:  # Encoder option
-            password = input('Please enter your password to encode: ')
-            encoded = encoder(password)  # storing the password encoded
-            print('Your password has been encoded and stored!')
-            print()
-
-        if user_input == 2:  # Decoder option
-            # sharing the encoded password and using the decoder function
-            print(f'The encoded password is {str(encoded)} and the original password is {str(decoded(encoded))}.')  #
-            print()
-
-        if user_input == 3:  # Exit function
-            break
-
-
-if __name__ == '__main__':  # Main
-    main()
+# hello
